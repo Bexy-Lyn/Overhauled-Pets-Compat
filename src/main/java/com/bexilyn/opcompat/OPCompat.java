0e9ad3a3f1389e1ecd3e0dd3256dc748046849b4
Bexy-Lyn <bexy_lyn@outlook.de>
2026-08-14 11:51:02 +0200

    setup dependencies

---
+++ src/main/java/com/bexilyn/opcompat/OPCompat.java
@@ -0,0 +1,11 @@
+package com.bexilyn.opcompat;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(OPCompat.MOD_ID)
+public class OPCompat {
+    public static final String MOD_ID = "opcompat";
+
+    public OPCompat() {
+    }
+}
